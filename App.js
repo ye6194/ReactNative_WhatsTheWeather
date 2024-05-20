3e1e99f67bfab23e4bca9fc47165259f3490d106
ye6194 <ye6194@naver.com>
2024-05-21 01:19:25 +0900

    Feat: init settings
    초기 세팅

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>김의진은 똥쟁이 똥멍청ㅇ</Text>
       <StatusBar style="auto" />
     </View>
   );
