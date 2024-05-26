61c0b661792dc03d3c9fdb13560f07ca9e23cdb3
ye6194 <ye6194@naver.com>
2024-05-26 18:45:44 +0900

    Design: 디자인 변경
    - 글자 색상 흰색으로 변경
    - 글자 크기 수정
    - 기온과 날씨 왼쪽으로 정렬

--- App.js
+++ App.js
@@ -1,6 +1,14 @@
 import { StatusBar } from "expo-status-bar";
 import { useEffect, useState } from "react";
-import { Platform, StyleSheet, Text, View, ScrollView, Dimensions } from "react-native";
+import {
+  Platform,
+  StyleSheet,
+  Text,
+  View,
+  ScrollView,
+  Dimensions,
+  ActivityIndicator,
+} from "react-native";
 import * as Location from "expo-location";
 
 // const { height, width } = Dimensions.get("window"); // 화면 크기 얻기
@@ -8,16 +16,22 @@ import * as Location from "expo-location";
 const { width: SCREEN_WIDTH } = Dimensions.get("window");
 console.log("SCREEN_WIDTH", SCREEN_WIDTH);
 
+const API_KEY = "d8fb21d6e7f63a694610873f153d9aff"; // 원래는 api 키를 앱에 두면 안됨
+
 export default function App() {
   const [city, setCity] = useState("Loading...");
-  const [location, setLocation] = useState();
+  // const [location, setLocation] = useState();
+  const [days, setDays] = useState([]);
   const [ok, setOk] = useState(true);
+
   // 요청을 보냄
-  const ask = async () => {
+  const getWeather = async () => {
     const { granted } = await Location.requestForegroundPermissionsAsync(); // forground - 앱 사용 중에만 위치를 사용
     if (!granted) {
       setOk(false);
+      // 요청을 보냈는데 유저가 허락하지 않았을 때 로직 짜보기
     }
+
     // prettier-ignore
     const {coords:{latitude, longitude}} = await Location.getCurrentPositionAsync({ accuracy: 5 });
     const location = await Location.reverseGeocodeAsync(
@@ -25,11 +39,17 @@ export default function App() {
       { useGoogleMaps: false }
     );
     setCity(location[0].city);
-    // console.log("location", location[0].city);
+
+    const response = await fetch(
+      `https://api.openweathermap.org/data/2.5/forecast?lat=${latitude}&lon=${longitude}&cnt=7&appid=${API_KEY}&units=metric`
+    );
+    const json = await response.json();
+    setDays(json.list);
+    // console.log("json.daily", json.list);
   };
 
   useEffect(() => {
-    ask();
+    getWeather();
   }, []);
 
   return (
@@ -42,25 +62,22 @@ export default function App() {
         pagingEnabled
         horizontal
         showsHorizontalScrollIndicator={false} // "false"로도 가능하지만 {false}로 쓰는게 더 좋음
-        // indicatorStyle="white"  // 안드로이드에서는 이 pros 작동X
+        // indicatorStyle="white"  // 안드로이드에서는 이 props 작동X
         contentContainerStyle={styles.weather}
       >
-        <View style={styles.day}>
-          <Text style={styles.temp}>27</Text>
-          <Text style={styles.description}>맑음</Text>
-        </View>
-        <View style={styles.day}>
-          <Text style={styles.temp}>27</Text>
-          <Text style={styles.description}>맑음</Text>
-        </View>
-        <View style={styles.day}>
-          <Text style={styles.temp}>27</Text>
-          <Text style={styles.description}>맑음</Text>
-        </View>
-        <View style={styles.day}>
-          <Text style={styles.temp}>27</Text>
-          <Text style={styles.description}>맑음</Text>
-        </View>
+        {days.length === 0 ? (
+          <View style={styles.day}>
+            <ActivityIndicator color="white" size="large" style={{ marginTop: 10 }} />
+          </View>
+        ) : (
+          days.map((day, index) => (
+            <View key={index} style={styles.day}>
+              <Text style={styles.temp}>{parseFloat(day.main.temp).toFixed(1)}</Text>
+              <Text style={styles.description}>{day.weather[0].main}</Text>
+              <Text style={styles.tinyText}>{day.weather[0].description}</Text>
+            </View>
+          ))
+        )}
       </ScrollView>
     </View>
   );
@@ -75,26 +92,35 @@ const styles = StyleSheet.create({
     flex: 1.2,
     justifyContent: "center",
     alignItems: "center",
-    // backgroundColor: "orange",
   },
   cityName: {
     fontSize: 58,
     fontWeight: "500",
+    color: "white",
   },
-  weather: {
-    // backgroundColor: "teal",
-  },
+  weather: {},
   day: {
     width: SCREEN_WIDTH,
-    alignItems: "center",
+    alignItems: "flex-start",
+    paddingHorizontal: 20,
+    // backgroundColor: "orange",
   },
   temp: {
     marginTop: 50,
-    fontSize: 178,
+    fontSize: 100,
     fontWeight: "600",
+    color: "white",
   },
   description: {
-    marginTop: -20,
-    fontSize: 60,
+    marginTop: -10,
+    fontSize: 30,
+    fontWeight: 500,
+    color: "white",
+  },
+  tinyText: {
+    marginTop: -5,
+    fontSize: 25,
+    fontWeight: 500,
+    color: "white",
   },
 });
